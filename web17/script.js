161c853f69287efc087966589b3645b98fc3185a
kawamugal <85334973+kawamugal@users.noreply.github.com>
2021-07-07 16:43:14 +0900

    Add files via upload

---
+++ web17/script.js
@@ -0,0 +1,16 @@
+$(function () {
+  // アコーディオンメニューのラベルがクリックされた場合
+    $('.menu-parent').on('hover', function () {
+
+    // ac-parentクラスの次の要素（ac-childクラス）の表示・非表示を切り替える
+    $(this).next().slideToggle();
+
+  //openクラスをつける
+    $(this).toggleClass("open");
+
+  //クリックされていないac-parentのopenクラスを取る
+    // $('.ac-parent').not(this).removeClass('open');
+  // 一つ開くと他は閉じるように
+    // $('.ac-parent').not($(this)).next('.ac-child').slideUp();
+  });
+});
