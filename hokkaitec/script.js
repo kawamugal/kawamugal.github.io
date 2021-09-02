9a31441d472a7e7de0a5d440ace115471f30c55b
kawamugal <85334973+kawamugal@users.noreply.github.com>
2021-09-02 16:10:57 +0900

    Add files via upload

--- hokkaitec/script.js
+++ hokkaitec/script.js
@@ -1,32 +1,15 @@
-$('a[href^="#"]').click(function() {
-  // スクロールの速度
-  let speed = 400; // ミリ秒で記述
-  let href = $(this).attr("href");
-  let target = $(href == "#" || href == "" ? 'html' : href);
-  let position = target.offset().top;
-  $('body,html').animate({
-    scrollTop: position
-  }, speed, 'swing');
-  return false;
+$(function(){
+	$(window).scroll(function (){
+		$('.fadein').each(function(){
+			var elemPos = $(this).offset().top + 0;//要素より0px下
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight + 100){
+				$(this).addClass('scrollin');// 画面内に入ったらscrollinというクラス名を追記
+			}
+      // else{
+      // $(this).removeClass('scrollin');// 画面外に出たらscrollinというクラス名を外す
+      // }
+		});
+	});
 });
-
-
-function fadeAnime(){
-
-  // ふわっ
-  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
-    var elemPos = $(this).offset().top+10;//要素より、10px下
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
-    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
-    }else{
-    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
-    }
-    });
-}
-
-// 画面をスクロールをしたら動かしたい場合の記述
-  $(window).scroll(function (){
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-  });// ここまで画面をスクロールをしたら動かしたい場合の記述
