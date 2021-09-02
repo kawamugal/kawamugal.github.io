436be745b5e8aea9f740c5346b19b3a59d62cc90
kawamugal <85334973+kawamugal@users.noreply.github.com>
2021-09-02 16:15:32 +0900

    Add files via upload

--- hokkaitec/script.js
+++ hokkaitec/script.js
@@ -4,7 +4,7 @@ $(function(){
 			var elemPos = $(this).offset().top + 0;//要素より0px下
 			var scroll = $(window).scrollTop();
 			var windowHeight = $(window).height();
-			if (scroll > elemPos - windowHeight + 100){
+			if (scroll > elemPos - windowHeight + 50){
 				$(this).addClass('scrollin');// 画面内に入ったらscrollinというクラス名を追記
 			}
       // else{
