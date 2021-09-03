e9725385a6801d27236aa74df4a02099e7da53a9
kawamugal <85334973+kawamugal@users.noreply.github.com>
2021-09-03 15:27:57 +0900

    Add files via upload

--- hokkaitec/script.js
+++ hokkaitec/script.js
@@ -1,10 +1,11 @@
 $(function(){
+	$(window).scrollTop(-1);// 1pxだけ下に自動スクロールさせる
 	$(window).scroll(function (){
 		$('.fadein').each(function(){
 			var elemPos = $(this).offset().top + 0;//要素より0px下
 			var scroll = $(window).scrollTop();
 			var windowHeight = $(window).height();
-			if (scroll > elemPos - windowHeight + 50){
+			if (scroll > elemPos - windowHeight + 0){
 				$(this).addClass('scrollin');// 画面内に入ったらscrollinというクラス名を追記
 			}
       // else{
