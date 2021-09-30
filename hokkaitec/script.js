1fea976f251bf16134da4b43a579f0098d82a9d9
kawamugal <85334973+kawamugal@users.noreply.github.com>
2021-09-30 09:32:03 +0900

    Add files via upload

--- hokkaitec/script.js
+++ hokkaitec/script.js
@@ -1,5 +1,4 @@
 $(function(){
-	$(window).scrollTop(-1);// 1pxだけ下に自動スクロールさせる
 	$(window).scroll(function (){
 		$('.fadein').each(function(){
 			var elemPos = $(this).offset().top + 0;//要素より0px下
@@ -8,9 +7,8 @@ $(function(){
 			if (scroll > elemPos - windowHeight + 0){
 				$(this).addClass('scrollin');// 画面内に入ったらscrollinというクラス名を追記
 			}
-      // else{
-      // $(this).removeClass('scrollin');// 画面外に出たらscrollinというクラス名を外す
-      // }
 		});
 	});
+
+	$(window).scroll();// 即時読み込ませる
 });
